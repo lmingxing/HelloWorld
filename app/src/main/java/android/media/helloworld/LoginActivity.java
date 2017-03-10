3abe52bfa96789435e1cc6a93b6dae57c2b90059
lmingxing <2992022660@qq.com>
2017-03-10 10:27:23 +0800

    添加Activity

---
+++ app/src/main/java/android/media/helloworld/LoginActivity.java
@@ -0,0 +1,14 @@
+package android.media.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("TAG", "onCreate: ");
+    }
+}
